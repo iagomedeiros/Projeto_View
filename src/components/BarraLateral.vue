a26a33d6b5d2580059d4487e6a9858fe5b36208f
Iago NM <iagonmedeiros@gmail.com>
2023-03-16 08:29:15 -0300

    criacao do formulario

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,34 @@
+ <!-- HTML -->
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+    </header>
+</template>
+
+<!-- JavaScript -->
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<!-- Todo estilo do componente -->
+<style scoped>
+header{
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+}
+
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style> 

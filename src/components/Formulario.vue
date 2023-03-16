08567665841f1cefe5bda800154b9c5add40cc20
Iago NM <iagonmedeiros@gmail.com>
2023-03-16 16:12:03 -0300

    criacao temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,23 +5,7 @@
                 <input type="text" class="input" placeholder="Qual a tarefa você deseja iniciar?">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>{{ tempoDecorrido }}</strong>
-                    </section>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+            <Temporizador/>    
 
             </div>
         </div>
@@ -29,30 +13,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, DefineComponent } from 'vue';
+import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
-    name: 'Formulário-Nome',
-    data(){
-        return{
-            tempoEmSegundos: 0,
-            cronometro: 0
-        }
-    },
-    computed:{
-tempoDecorrido(): string{
-    return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-}
-    },
-    methods: {
-        iniciar(){
-            this.cronometro = setInterval(()=> {
-                this.tempoEmSegundos += 1
-            },1000)
-        },
-        finalizar (){
-        clearInterval(this.cronometro)    
-        }
+    name: "Formulário-nome",
+    components:{
+        Temporizador
     }
-});
+})
 </script>
